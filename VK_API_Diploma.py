f1d8db7f60fdc0e71f82fa435cab8e7cd0d12df1
Alex Panyushkin <panyushkin91@mail.ru>
2020-06-29 19:59:31 +0500

    Update code

--- VK_API_Diploma.py
+++ VK_API_Diploma.py
@@ -4,27 +4,45 @@ import json
 import time
 
 TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-user_id = input('Введите ID пользователя ')
+USER_ID = input('Введите ID пользователя ')
 
 
 class User:
 
-    def __init__(self, user_id):
-        self.user_id = user_id
+    def __init__(self, USER_ID):
+        if str(USER_ID).isdigit():
+            self.user_id = USER_ID
+        else:
+            time.sleep(1 / 3)
+            self.params = {
+                'access_token': TOKEN,
+                'v': 5.89,
+                'screen_name': USER_ID
+            }
+            response = requests.get(
+                'https://api.vk.com/method/utils.resolveScreenName',
+                params=self.params
+            ).json()
+            self.USER_ID = response['response']['object_id']
         self.params = {
             'access_token': TOKEN,
             'v': 5.89,
-            'user_id': self.user_id
+            'user_id': self.user_id,
         }
 
     def get_user_groups(self):
+        self.params['extended'] = 1
+        self.params['fields'] = 'members_count'
         response = requests.get(
-            'https://api.vk.com/method/users.getSubscriptions',
+            'https://api.vk.com/method/groups.get',
             params=self.params).json()
-        groups_set = [item for item in response['response']['groups']['items']]
+        return response
+
+    def group_set(self):
+        groups_set = [item['id'] for item in User.get_user_groups(self)['response']['items']]
         return groups_set
 
-    def get_user_friends(self):
+    def get_friends(self):
         response = requests.get(
             'https://api.vk.com/method/friends.get',
             params=self.params
@@ -33,83 +51,46 @@ class User:
         return friends_id
 
 
-def check_groups(friends, users):
-    return bool(set(friends) & set(users))
-
-
-def get_group_members():
-    processing_counter = 1
-    counter = 1000
-    offset = 0
+def uncommon_group(user_id):
+    main_user = User(user_id)
     time.sleep(1/3)
-    friends = user.get_user_friends()
+    friends_set = main_user.get_friends()
     time.sleep(1/3)
-    groups = user.get_user_groups()
-    new_groups = groups.copy()
-    print('Checking user groups')
-    for group in groups:
-        group_member_ids = []
-        group_id = group
-        params = {
-            'access_token': TOKEN,
-            'v': 5.89,
-            'count': counter,
-            'offset': offset,
-            'group_id': group_id
-        }
+    group_set = main_user.group_set()
+    print('Checking friends and their groups . . .')
+    counter = 1
+    for friend_id in friends_set:
+        user = User(friend_id)
+        time.sleep(1/3)
         try:
-            while True:
-                time.sleep(1/3)
-                response = requests.get(
-                    'https://api.vk.com/method/groups.getMembers',
-                    params=params
-                ).json()
-                group_member_ids.extend(response['response']['items'])
-                if len(response['response']['items']) < 1000:
-                    break
-                else:
-                    params['offset'] += counter
-                print('Requesting API . . .')
+            new_group_set = user.group_set()
         except KeyError:
-            pass
-        if check_groups(friends, group_member_ids):
-            new_groups.remove(group)
-        print(f'{processing_counter}/{len(groups)}')
-        processing_counter += 1
-    return new_groups
+            print(f'Пользователь id{friend_id} имеет закрытый профиль или заблокирован')
+            print(f'{counter}/{len(friends_set)}')
+            counter += 1
+            continue
+        group_set = set(group_set) - set(new_group_set)
+        print(f'{counter}/{len(friends_set)}')
+        counter += 1
+    return group_set
 
 
 def get_data():
     data = []
-    try:
-        for group in get_group_members():
-            params = {
-                'access_token': TOKEN,
-                'v': 5.89,
-                'group_id': group,
-                'fields': 'members_count'
-            }
-            time.sleep(1/3)
-            response = requests.get(
-                'https://api.vk.com/method/groups.getById',
-                params=params
-            ).json()
-            data_group = {
-                'name': response['response'][0]['name'],
-                'gid': response['response'][0]['id'],
-                'members_count': response['response'][0]['members_count']
-            }
-            data.append(data_group)
-    except KeyError:
-        pass
+    user = User(USER_ID)
+    unique_groups = uncommon_group(USER_ID)
+    for group in user.get_user_groups()['response']['items']:
+        if group['id'] in unique_groups:
+            try:
+                data.append({'name': group['name'], 'gid': group['id'], 'members_count': group['members_count']})
+            except KeyError:
+                continue
     return data
 
 
 def dump_json():
-    with open('groups.json', 'w', encoding='UTF-8') as fo:
+    with open('groups.json', 'w', encoding='utf-8') as fo:
         json.dump(get_data(), fo, ensure_ascii=False, indent=2)
 
-
-user = User(user_id)
 
 dump_json()
